e49cf2a586deccaf7a911bfc98887888eca17699
Hanish Tharwani <104623869+hanishtharwani123@users.noreply.github.com>
2023-11-30 00:49:44 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(
   cors({
-    origin: ["https://task-wise-mern-frontend.vercel.app/"],
+    origin: ["https://task-wise-mern-frontend.vercel.app"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
