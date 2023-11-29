f15da344727734f24e20b5a5377b7d4b5abb9d48
Hanish Tharwani <104623869+hanishtharwani123@users.noreply.github.com>
2023-11-30 00:47:59 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -9,7 +9,13 @@ const port = process.env.PORT || 5000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use(cors());
+app.use(
+  cors({
+    origin: ["https://task-wise-mern-frontend.vercel.app/"],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
 
 app.get("/get", async (req, res) => {
   try {
