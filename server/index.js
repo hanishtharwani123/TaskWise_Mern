57414a58d66d272292f2136d11238fbdfd721281
Hanish Tharwani <hanish.tharwani2021@vitbhopal.ac.in>
2023-12-04 18:56:01 +0530

    first commit

--- server/index.js
+++ server/index.js
@@ -9,13 +9,7 @@ const port = process.env.PORT || 5000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use(
-  cors({
-    origin: ["https://task-wise-mern-frontend.vercel.app"],
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    credentials: true,
-  })
-);
+app.use(cors());
 
 app.get("/get", async (req, res) => {
   try {
