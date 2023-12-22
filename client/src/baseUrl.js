c6fa2a0ab312bd99cf9e22b2c57e151ef5174da2
Hanish Tharwani <hanish.tharwani2021@vitbhopal.ac.in>
2023-12-22 23:29:42 +0530

    first commit

--- client/src/baseUrl.js
+++ client/src/baseUrl.js
@@ -1,4 +1,4 @@
 // const baseUrl = "http://localhost:5000";
-const baseUrl = "https://taskwise-mern.onrender.com";
+const baseUrl = "https://task-wise-hanish.onrender.com";
 
 export default baseUrl;
